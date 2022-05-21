c7d8f9212efa2d79ccaf1ad323e1749819ed03f2
Manuel Luna <manuellunada@gmail.com>
2022-05-20 21:40:57 -0500

    welcome screen

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,28 @@
+import { motion } from "framer-motion";
+import styles from "./Home.module.css";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <motion.div animate={{ margin: "3rem" }} className={styles.container}>
+      <h1>Welcome!</h1>
+
+      <form>
+        <div className={styles.inputGroup}>
+          <label htmlFor="room-name">Room name</label>
+          <input type="text" id="room-name" />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <label htmlFor="player-name">Player name</label>
+          <input type="text" id="player-name" />
+        </div>
+
+        <div className={styles.playContainer}>
+          <button>Play</button>
+        </div>
+      </form>
+    </motion.div>
+  );
 };
 
 export default Home;
