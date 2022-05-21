14885de6ad457d853240638c83c906fe4a83694f
Manuel Luna <manuellunada@gmail.com>
2022-05-20 22:32:51 -0500

    validation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,21 +2,51 @@ import { motion } from "framer-motion";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import styles from "./Home.module.css";
 
 const SERVER_URL: string = process.env.SERVER_URL ?? "http://localhost:4000";
 
+type Inputs = {
+  roomName: string;
+  playerName: string;
+};
+
+const schema = yup
+  .object({
+    roomName: yup
+      .string()
+      .required("Required")
+      .matches(/^[A-Z ]+$/i, "Should only contain letters and spaces")
+      .max(30, "Should be at most 30 characters long")
+      .min(4, "Should be at least 4 characters long"),
+    playerName: yup
+      .string()
+      .required("Required")
+      .matches(/^[A-Z ]+$/i, "Should only contain letters and spaces")
+      .max(30, "Should be at most 30 characters long")
+      .min(4, "Should be at least 4 characters long"),
+  })
+  .required();
+
 const Home = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>({ resolver: yupResolver(schema) });
+
   const navigate = useNavigate();
+  console.log(isSubmitting);
 
-  const startGameHandler = async (e: React.SyntheticEvent) => {
-    e.preventDefault();
-
+  const startGameHandler: SubmitHandler<Inputs> = async (data) => {
     try {
       const response = await axios.post(`${SERVER_URL}/api/rooms`, {
         room: {
-          name: "Friends",
-          first: { name: "Mari" },
+          name: data.roomName,
+          first: { name: data.playerName },
         },
       });
 
@@ -32,19 +62,39 @@ const Home = () => {
     <motion.div animate={{ margin: "3rem" }} className={styles.container}>
       <h1>Welcome!</h1>
 
-      <form onSubmit={startGameHandler}>
+      <form onSubmit={handleSubmit(startGameHandler)}>
         <div className={styles.inputGroup}>
           <label htmlFor="room-name">Room name</label>
-          <input type="text" id="room-name" />
+          <input
+            type="text"
+            id="room-name"
+            {...register("roomName", {
+              required: true,
+              maxLength: 30,
+              pattern: /^[A-Z]+$/i,
+              minLength: 4,
+            })}
+          />
+          {errors.roomName && <span>{errors.roomName?.message}</span>}
         </div>
 
         <div className={styles.inputGroup}>
           <label htmlFor="player-name">Player name</label>
-          <input type="text" id="player-name" />
+          <input
+            type="text"
+            id="player-name"
+            {...register("playerName", {
+              required: true,
+              maxLength: 30,
+              pattern: /^[A-Z]+$/i,
+              minLength: 4,
+            })}
+          />
+          {errors.playerName && <span>{errors.roomName?.message}</span>}
         </div>
 
         <div className={styles.playContainer}>
-          <button>Play</button>
+          <button disabled={isSubmitting}>Play</button>
         </div>
       </form>
     </motion.div>
