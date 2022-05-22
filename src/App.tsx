8ab76e92d14e16392bf53229c69391a5b68789a0
Manuel Luna <manuellunada@gmail.com>
2022-05-22 16:18:06 -0500

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import "react-toastify/dist/ReactToastify.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Room from "./components/Room";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
       <ToastContainer position="top-center" pauseOnHover theme="colored" />
       <div>
-        <Link to="/">Home</Link>
+        <nav className={styles.nav}>
+          <Link className={styles.homeLink} to="/">
+            Home
+          </Link>
+        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/:roomId" element={<Room />} />
