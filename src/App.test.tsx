eee482e666f846e30ab56993799901f23b9b4e3b
Manuel Luna <manuellunada@gmail.com>
2022-06-14 18:43:21 -0500

    configuration

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { test, expect } from "@playwright/test";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("basic test", async ({ page }) => {
+  await page.goto("http://localhost:3000");
 });
