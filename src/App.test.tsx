ddb517ae243fb5020e446fa6c1a97c3c4012d58d
Manuel Luna <manuellunada@gmail.com>
2022-06-14 18:52:35 -0500

    homepage test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-test("basic test", async ({ page }) => {
+test("homepage", async ({ page }) => {
   await page.goto("http://localhost:3000");
+
+  await expect(page.locator('text=Choose').first()).toBeVisible()
+
+	await page.type('#room-name', "Secret Base")
+	await page.type('#player-name', "Kanna")
+	await page.click('text=Create game')
+
+  await expect(page.locator('text=Feeling lonely?').first()).toBeVisible()
 });
