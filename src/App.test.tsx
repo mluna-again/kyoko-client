c69a4c36e22f168a7fa2f9755416e7dc3abe6721
Manuel Luna <manuellunada@gmail.com>
2022-06-14 19:40:35 -0500

    some tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -15,18 +15,13 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("create game", () => {
-  test("user can create room", async ({ page }) => {
+  test("user can create room and enter it", async ({ page }) => {
     await expect(page.locator(`text=${PLAYER_NAME}`)).toBeVisible();
   });
 });
 
-test.describe("join game", () => {
-  test("user can copy an invitation link", async ({ page }) => {
-    await page.locator("text=Invite your friends").click();
-    await expect(page.locator("text=Link copied!")).toBeVisible();
-  });
-
-  test("new users can join room", async ({ page, browser }) => {
+test.describe("play game", () => {
+  test("user can copy an invitation link", async ({ page, browser }) => {
     const newContext = await browser.newContext();
     const secondaryPage = await newContext.newPage();
     await secondaryPage.goto(page.url());
@@ -38,5 +33,36 @@ test.describe("join game", () => {
     await expect(page.locator("text=Yukiji")).toBeVisible();
     await expect(secondaryPage.locator("text=Kanna")).toBeVisible();
     await expect(secondaryPage.locator("text=Yukiji")).toBeVisible();
+    await page.locator("text=Invite your friends").click();
+    await expect(page.locator("text=Link copied!")).toBeVisible();
   });
+
+  test("reveal cards", async ({ page, browser }) => {
+    const newContext = await browser.newContext();
+    const secondaryPage = await newContext.newPage();
+    await secondaryPage.goto(page.url());
+    await expect(secondaryPage.locator("text=Enter your name")).toBeVisible();
+    await secondaryPage.type(".swal2-input", "Yukiji");
+    await secondaryPage.click(".swal2-confirm");
+
+    await expect(page.locator("text=Kanna")).toBeVisible();
+    await expect(page.locator("text=Yukiji")).toBeVisible();
+    await expect(secondaryPage.locator("text=Kanna")).toBeVisible();
+    await expect(secondaryPage.locator("text=Yukiji")).toBeVisible();
+
+		await expect(page.locator("text=Pick your cards!")).toBeVisible()
+		await expect(secondaryPage.locator("text=Pick your cards!")).toBeVisible()
+
+    await secondaryPage.click("text=34");
+		await page.click("text=0")
+
+		await expect(page.locator("text=Reveal cards")).toBeVisible()
+		await expect(secondaryPage.locator("text=Reveal cards")).toBeVisible()
+
+    await page.pause();
+  });
+  // test("game over")
+  // test("average message")
+  // test("same answer message")
+  // test("reset game")
 });
