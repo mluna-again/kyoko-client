b924e5ec815329a3aedc5d3098aa675e248e40bc
Manuel Luna <manuellunada@gmail.com>
2022-06-14 19:16:56 -0500

    more tessts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -18,4 +18,23 @@ test.describe("whole app", () => {
   test("user can create room", async ({ page }) => {
     await expect(page.locator(`text=${PLAYER_NAME}`)).toBeVisible();
   });
+
+  test("user can copy an invitation link", async ({ page }) => {
+    await page.locator("text=Invite your friends").click();
+    await expect(page.locator("text=Link copied!")).toBeVisible();
+  });
+
+  test("new users can join room", async ({ page, browser }) => {
+    const newContext = await browser.newContext();
+    const secondaryPage = await newContext.newPage();
+    await secondaryPage.goto(page.url());
+    await expect(secondaryPage.locator("text=Enter your name")).toBeVisible();
+    await secondaryPage.type(".swal2-input", "Yukiji");
+    await secondaryPage.click(".swal2-confirm");
+
+    await expect(page.locator("text=Kanna")).toBeVisible();
+    await expect(page.locator("text=Yukiji")).toBeVisible();
+    await expect(secondaryPage.locator("text=Kanna")).toBeVisible();
+    await expect(secondaryPage.locator("text=Yukiji")).toBeVisible();
+  });
 });
