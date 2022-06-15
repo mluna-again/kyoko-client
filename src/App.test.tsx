1e5f6acdce9c6100290124ad3455cd4efdf9e3ef
Manuel Luna <manuellunada@gmail.com>
2022-06-14 19:19:20 -0500

    refactor 😑👍

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,23 +2,25 @@ import { test, expect } from "@playwright/test";
 
 const PLAYER_NAME = "Kanna";
 
-test.describe("whole app", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:3000");
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:3000");
 
-    await expect(page.locator("text=Choose").first()).toBeVisible();
+  await expect(page.locator("text=Choose").first()).toBeVisible();
 
-    await page.type("#room-name", "Secret Base");
-    await page.type("#player-name", PLAYER_NAME);
-    await page.click("text=Create game");
+  await page.type("#room-name", "Secret Base");
+  await page.type("#player-name", PLAYER_NAME);
+  await page.click("text=Create game");
 
-    await expect(page.locator("text=Feeling lonely?").first()).toBeVisible();
-  });
+  await expect(page.locator("text=Feeling lonely?").first()).toBeVisible();
+});
 
+test.describe("create game", () => {
   test("user can create room", async ({ page }) => {
     await expect(page.locator(`text=${PLAYER_NAME}`)).toBeVisible();
   });
+});
 
+test.describe("join game", () => {
   test("user can copy an invitation link", async ({ page }) => {
     await page.locator("text=Invite your friends").click();
     await expect(page.locator("text=Link copied!")).toBeVisible();
