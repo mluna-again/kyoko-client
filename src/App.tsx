19e73822e15d1c78535bcea7161ea9e48368839b
Manuel Luna <manuellunada@gmail.com>
2022-06-15 16:47:54 -0500

    dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { ToastContainer } from "react-toastify";
+import cx from "classnames";
 import "react-toastify/dist/ReactToastify.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { Routes, Route, Link } from "react-router-dom";
@@ -7,14 +9,22 @@ import Room from "./components/Room";
 import styles from "./App.module.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <>
       <ToastContainer position="top-right" pauseOnHover theme="colored" />
-      <div>
+      <div className={cx(styles.container, { [styles.dark]: darkMode })}>
         <nav className={styles.nav}>
           <Link className={styles.homeLink} to="/">
             <span>🃖</span> Kyoko
           </Link>
+          <button
+            className={styles.darkMode}
+            onClick={() => setDarkMode(!darkMode)}
+          >
+            🌕
+          </button>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
