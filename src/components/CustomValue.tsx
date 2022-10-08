ebc3913b862a5f1ab55ddf3cd95e95fdf74c0e91
Manuel Luna <manuellunada@gmail.com>
2022-10-07 21:37:19 -0500

    emoji

--- src/components/CustomValue.tsx
+++ src/components/CustomValue.tsx
@@ -7,31 +7,39 @@ type Props = {
 };
 
 const CustomValue = ({ onConfirm, gameOver }: Props) => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState("");
   const updateValue = (event: React.FormEvent<HTMLInputElement>) => {
-    const value = Number(event.currentTarget.value);
-    if (event.currentTarget.value === "") return setValue(0);
-    if (!value) return;
+    const value = event.currentTarget.value;
     if (gameOver) return;
 
     setValue(value);
   };
 
+  const isValidNumber = Boolean(Number(value));
+  const isNegative = isValidNumber && Number(value) < 0;
+  const isTooBiG = isValidNumber && Number(value) > 100;
+  const shouldDisplayEmoji =
+    Boolean(value && !isValidNumber) || isNegative || isTooBiG;
+
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (gameOver) return;
-    onConfirm("❓", value);
+    if (!isValidNumber || isNegative) return;
+    onConfirm("❓", Number(value));
   };
 
   return (
     <div className={styles.container}>
       <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          value={value}
-          onChange={updateValue}
-          disabled={gameOver}
-        />
+        <div className={styles.inputContainer}>
+          {shouldDisplayEmoji && <span className={styles.emoji}>🤨</span>}
+          <input
+            type="text"
+            value={value}
+            onChange={updateValue}
+            disabled={gameOver}
+          />
+        </div>
         <button>Confirm</button>
       </form>
     </div>
