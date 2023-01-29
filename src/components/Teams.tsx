4d3f75f293d94f2f76a0d336a68302e53e43a3f6
Manuel Luna <manuellunada@gmail.com>
2023-01-28 20:25:30 -0600

    styles

--- src/components/Teams.tsx
+++ src/components/Teams.tsx
@@ -3,28 +3,28 @@ import styles from "./Teams.module.css";
 type Props = { setTeam: (team: string) => void; team?: string };
 const Teams = ({ setTeam, team }: Props) => {
   return (
-    <div>
-      <h2>Select a team!</h2>
+    <div className={styles.container}>
+      <h2>Select a team</h2>
 
-      <div>
+      <div className={styles.buttons}>
         <button
           type="button"
           onClick={() => setTeam("white")}
-          className={`${styles.teamButton} ${
-            team === "white" ? "selected" : ""
+          className={`${styles.teamButton} ${styles.white} ${
+            team === "white" ? styles.selected : ""
           }`}
         >
-          White
+          <img src="queen.png" alt="chess queen" />
         </button>
 
         <button
           type="button"
           onClick={() => setTeam("black")}
-          className={`${styles.teamButton} ${
-            team === "black" ? "selected" : ""
+          className={`${styles.teamButton} ${styles.black} ${
+            team === "black" ? styles.selected : ""
           }`}
         >
-          Black
+          <img src="queen.png" alt="chess queen" />
         </button>
       </div>
     </div>
