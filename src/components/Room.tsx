dc86dbba6003ee35da7d5ec12a9d43676b02537d
Manuel Luna <manuellunada@gmail.com>
2023-01-28 18:47:37 -0600

    refactor

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,79 +1,45 @@
 import { useParams, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Swal from "sweetalert2";
+import { useRef } from "react";
 import { Socket } from "phoenix";
-import axios from "axios";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { toast } from "react-toastify";
 import useRoomChannel from "../hooks/useRoomChannel";
 import Board from "./Board";
-import { RoomType } from "../constants/types";
-import { SERVER_URL, SERVER_SOCKET_URL } from "../constants/values";
+import { SERVER_SOCKET_URL } from "../constants/values";
+import { useRoomInfo } from "../hooks/useRoomInfo";
 import styles from "./Room.module.css";
 
 const socket = new Socket(SERVER_SOCKET_URL);
 socket.connect();
-// socket.onOpen(() => console.log("Socket connected"));
-// socket.onClose(() => console.log("Socket disconnected"));
-// socket.onError(console.error);
 
 const Room = () => {
   const { state } = useLocation();
-  const [playerName, setPlayerName] = useState(
+  const playerName = useRef(
     (state as any)?.player || localStorage.getItem("user")
   );
 
-  const [room, setRoom] = useState<RoomType>();
+  const params = useParams();
+  const { room, error: roomError } = useRoomInfo(params.roomId!);
   const {
     channel,
     users,
     error: channelError,
-  } = useRoomChannel(socket, room, playerName);
-
-  const params = useParams();
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `${SERVER_URL}/api/rooms/${params.roomId}`
-        );
-
-        setRoom(response.data.data);
-        let pName = playerName;
-        while (!Boolean(pName)) {
-          const { value } = await (Swal.fire as any)({
-            confirmButtonColor: "var(--primary)",
-            title: "Enter your name",
-            input: "text",
-            inputValue: "",
-            inputValidator: (value: string) => {
-              if (!value) {
-                return "Invalid username!";
-              }
-              if (value.length > 30) {
-                return "Username must not be longer than 30 characters!";
-              }
-            },
-          });
-          pName = value;
-        }
-        localStorage.setItem("user", pName);
-        setPlayerName(pName);
-      } catch (error) {
-        console.error(error);
-        alert("Invalid room!");
-      }
-    };
-
-    fetchData();
-  }, [params.roomId, playerName]);
+  } = useRoomChannel(socket, room, playerName.current);
 
   const copyLinkHandler = () => {
     toast("Link copied!", { type: "success" });
   };
 
-  if (!room || !channel) return <h1>Loading...</h1>;
   if (channelError) return <h1>Invalid room...</h1>;
+  if (roomError) return <h1>{roomError}</h1>;
+
+  // no username selected
+  if (!room || !channel)
+    return (
+      <div>
+        <h1>Select a username</h1>
+      </div>
+    );
 
   return (
     <div className={styles.container}>
@@ -90,7 +56,7 @@ const Room = () => {
       <div>
         <Board
           initialState={room}
-          playerName={playerName}
+          playerName={playerName.current}
           channel={channel}
           users={users}
         />
---
+++ src/hooks/useRoomInfo.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { SERVER_URL } from "../constants/values";
+import { RoomType } from "../constants/types";
+
+type useRoomInfo = (roomId: string) => {
+  room: RoomType | undefined;
+  error: string | null;
+};
+export const useRoomInfo: useRoomInfo = (roomId: string) => {
+  const [room, setRoom] = useState<RoomType>();
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get(`${SERVER_URL}/api/rooms/${roomId}`)
+      .then((response) => setRoom(response.data.data))
+      .catch(() => setError("Server says: No"));
+  }, [roomId]);
+
+  return { room, error };
+};
