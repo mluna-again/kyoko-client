9a60c02f7cdb12a0e70b3fc3af1b84d67e3ff75a
Manuel Luna <manuellunada@gmail.com>
2023-03-27 21:34:34 -0600

    issue item styles

--- src/components/Issue.tsx
+++ src/components/Issue.tsx
@@ -1,27 +1,36 @@
+import cx from "classnames";
 import { Issue as IssueType } from "../constants/types";
+import styles from "./Issue.module.css";
 
 type Props = {
-	children: IssueType;
-	setAsActive: (issue: IssueType|null) => void;
-	active: IssueType|null;
-}
+  children: IssueType;
+  setAsActive: (issue: IssueType | null) => void;
+  active: IssueType | null;
+};
 const Issue = ({ children, setAsActive, active }: Props) => {
-	const beingVoted = active?.id === children.id;
-	const onVoteHandler = () => {
-		if (beingVoted) {
-			setAsActive(null);
-			return
-		}
+  const beingVoted = active?.id === children.id;
+  const onVoteHandler = () => {
+    if (beingVoted) {
+      setAsActive(null);
+      return;
+    }
 
-		setAsActive(children);
-	};
+    setAsActive(children);
+  };
 
-	const message = beingVoted ? "Voting" : "Vote";
+  const message = beingVoted ? "Stop voting" : "Vote";
+  const buttonClasses = cx(styles.button, {
+    [styles.voting]: beingVoted,
+  });
 
-	return <div>
-		<h1>{children.title}</h1>
-		<button onClick={onVoteHandler}>{message}</button>
-	</div>
-}
+  return (
+    <div className={styles.container}>
+      <h1>{children.title}</h1>
+      <button onClick={onVoteHandler} className={buttonClasses}>
+        {message}
+      </button>
+    </div>
+  );
+};
 
-export default Issue
+export default Issue;
